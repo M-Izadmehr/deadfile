ce1748b22f34173a0767637c5251a8efc53bc9e2
Mojtaba Izadmehr <m.eezadmehr@gmail.com>
2019-10-29 23:27:21 +0100

    chore(docs): remove unused images

--- docs/images/supportedExports.js
+++
@@ -1,6 +0,0 @@
-// Example of supported Export aggregation
-export * from "path"; 
-export * as name1 from "path";
-export { name1, name2, nameN } from "path";
-export { import1 as name1, import2 as name2, nameN } from "path";
-export { default } from "path";
