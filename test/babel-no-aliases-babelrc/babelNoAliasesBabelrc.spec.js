f6f258331b15f5c390e65424e8d13690e28bf3f1
Kevin Barabash <kevinb7@gmail.com>
2022-06-22 13:22:10 -0400

    Fix #45: Update checkBabelModuleResolver to handle no module-resolver plugin (#46)
    Fixes the issue when no babel module resolver exists

--- test/babel-no-aliases-babelrc/babelNoAliasesBabelrc.spec.js
+++ test/babel-no-aliases-babelrc/babelNoAliasesBabelrc.spec.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const timeout = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-test("babel-module-resolver-babelrc", async () => {
+test("babel-no-aliases-babelrc", async () => {
   const argv = {
     _: [path.join(__dirname, "./src/index.js")],
     dir: path.join(__dirname, "./"),
@@ -14,7 +14,7 @@ test("babel-module-resolver-babelrc", async () => {
   // TODO: refactor code for proper await
   await timeout(1000);
 
-  expect(Object.keys(analysis.allAssets).length).toBe(4);
+  expect(Object.keys(analysis.allAssets).length).toBe(3);
   expect(
     Object.keys(analysis.allAssets).some(item => item.match("directImport"))
   ).toBe(true);
@@ -22,26 +22,18 @@ test("babel-module-resolver-babelrc", async () => {
     Object.keys(analysis.allAssets).some(item => item.match("index"))
   ).toBe(true);
   expect(
-    Object.keys(analysis.allAssets).some(item => item.match("aliasedImport"))
-  ).toBe(true);
-  expect(
     Object.keys(analysis.allAssets).some(item => item.match("unused"))
   ).toBe(true);
 
-  expect([...analysis.usedAssets].length).toBe(4);
+  expect([...analysis.usedAssets].length).toBe(2);
   expect(
     [...analysis.usedAssets].some(item => item[0].match("directImport"))
   ).toBe(true);
   expect([...analysis.usedAssets].some(item => item[0].match("index"))).toBe(
     true
   );
-  expect(
-    [...analysis.usedAssets].some(item => item[0].match("aliasedImport"))
-  ).toBe(true);
-  expect([...analysis.usedAssets].some(item => item[0].match("lodash"))).toBe(
-    true
-  );
 
-  expect([...analysis.notFound].length).toBe(1);
+  expect([...analysis.notFound].length).toBe(2);
   expect([...analysis.notFound][0]).toBe("react");
+  expect([...analysis.notFound][1]).toBe("underscore");
 });
---
+++ test/babel-no-aliases-babelrc/src/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+
+import React from 'react'
+import directImport from './directImport';
+import _ from 'underscore';
